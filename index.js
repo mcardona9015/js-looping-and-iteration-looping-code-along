ea2849df7f6262ace2ce2f50fd04bf6926eb0c76
mcardona9015 <mcardona9015@gmail.com>
2021-01-30 22:33:06 -0500

    commit tree 3bce7005cc28f33c9c758b91849f0883be0f864a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards( cards, event ) {
+    for (let index = 0; index < cards.length; index++) {
+
+        console.log(`Thank you, ${cards[index]}, for the wonderful ${event} gift!`);
+   
+    }
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i);
+        i = i - 1;
+    } 
+}
